85e52428054896e36c9322c848ccb0d900df698a
ttedgar <150164985+ttedgar@users.noreply.github.com>
2024-08-22 13:33:54 +0200

    build: create InstructorController class with endpoints

---
+++ backend/rate-my-driving-instructor-backend/src/main/java/com/codecool/ratemydrivinginstructorbackend/controller/InstructorController.java
@@ -0,0 +1,34 @@
+package com.codecool.ratemydrivinginstructorbackend.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class InstructorController {
+    private InstructorService instructorService;
+
+    public InstructorController(InstructorService instructorService) {
+        this.instructorService = instructorService;
+    }
+
+    @GetMapping("/instructor")
+    public List<InstructorDTO> getAllinstructors() {
+        return instructorService.getAllInstructors();
+    }
+
+    @GetMapping("/instructor/{instructorId}")
+    public InstructorDTO getInstructor(@PathVariable int instructorId) {
+        return instructorService.getInstructor(instructorId);
+    }
+
+    @PostMapping("/instructor")
+    public void createInstructor(@RequestBody InstructorDTO instructor) {
+        instructorService.createInstructor(instructor);
+    }
+
+    @PutMapping("/instructor/{instrucorId}")
+    public void updateInstructor(@PathVariable int instrucorId, @RequestBody InstructorDTO instructor) {
+        instructorService.updateInstructor(instrucorId, instructor);
+    }
+}
