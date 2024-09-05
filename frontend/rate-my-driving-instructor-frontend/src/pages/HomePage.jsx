181c5869a2264f8333808e987ec71afad88b7844
bence <bence.matuz1@gmail.com>
2024-09-05 14:02:25 +0200

    fix: stats object

--- frontend/rate-my-driving-instructor-frontend/src/pages/HomePage.jsx
+++ frontend/rate-my-driving-instructor-frontend/src/pages/HomePage.jsx
@@ -14,28 +14,33 @@ export default function HomePage() {
     }
 
     async function fetchAllStatData() {
-        const schoolResponse = await fetch(`/api/school/${searchType}/${searchItem}`);   
-        const instructorNumber = await fetch(`/api/school/${searchType}/${searchItem}`);
-        const reviewNumber = await fetch(`/api/school/${searchType}/${searchItem}`);
-        const userNumber = await fetch(`/api/school/${searchType}/${searchItem}`);
+        const schoolRes = await fetch(`/api/school/count`);   
+        const instructorRes = await fetch(`/api/instructor/count`);
+        const reviewRes = await fetch(`/api/review/count`);
+        const userRes = await fetch(`/api/reviwer/count`);
 
-        const schoolResult = await schoolNumber.json();
-        const instructorResult = await instructorNumber.json();
-        const reviewResult = await reviewNumber.json();
-        const userResult = await userNumber.json();
-        
-        setStats({
-            "school": schoolNumber,
-            "instructor": instructorNumber,
-            "review": reviewNumber,
-            "user": userNumber
-        })
+        const schoolNumber = await schoolRes.json();
+        const instructorNumber = await instructorRes.json();
+        const reviewNumber = await reviewRes.json();
+        const userNumber = await userRes.json();
+
+        // if (schoolNumber || instructorNumber || reviewNumber || userNumber) {
+        //     setStats({
+        //         "school": schoolNumber,
+        //         "instructor": instructorNumber,
+        //         "review": reviewNumber,
+        //         "user": userNumber
+        //     })
+        // } 
+
     }
 
     function onSubmit(event) {
         event.preventDefault();
         fetchSearchData(searchType, searchItem);
     }
+
+    function onClickToggle()
 
     useEffect(() => {
         fetchAllStatData()
@@ -54,9 +59,12 @@ export default function HomePage() {
                 />
             </form>
             <div className="searchButtons">
-                <button type="button" 
+                <button 
+                type="button" 
                 onClick={() => setSearchType("instructor")}>Instructor</button>
-                <button type="button" onClick={() => setSearchType("school")}>School</button> 
+                <button 
+                type="button" 
+                onClick={() => setSearchType("school")}>School</button> 
             </div>
         
             <div className="searchResults">    
@@ -77,17 +85,17 @@ export default function HomePage() {
         <div className="statDiv">STATS
             <div className="schoolBox">
                 Number of schools: 
-                {fetchAllStatData[school]}
+                {stats["school"]}
             </div>
             <div className="intructorBox">
             Number of instructors: 
-            {fetchAllStatData[instructor]}
+            {stats["instructor"]}
             </div>
             <div className="userBox">
-            {fetchAllStatData[user]}
+            {stats["user"]}
             </div>
             <div className="reviewBox">
-            {fetchAllStatData[review]}
+            {stats["review"]}
             </div>
         </div>
         </>
