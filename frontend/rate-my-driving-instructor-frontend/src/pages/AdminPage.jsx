99cbdd87c530170dc4f0b896b00803f78cb650a6
judit-g <108220413+judit-g@users.noreply.github.com>
2024-10-02 18:30:48 +0200

    'feat: add AdminPage'

--- frontend/rate-my-driving-instructor-frontend/src/pages/AdminPage.jsx
+++ frontend/rate-my-driving-instructor-frontend/src/pages/AdminPage.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SearchForm from './../component/SearchForm';
+import UserPage from './../component/UserPage';
 
 function AdminPage() {
 
+  const [searchedUserName, setSearchedUserName] = useState("");
+  const [userSearchResult, setUserSearchResult] = useState(null);
+
+  async function fetchUser() {
+    const response = await fetch(`/api/admin/appuser?username=${searchedUserName}`, {
+      method: 'GET',
+      headers: {
+          'Authorization': `Bearer ${localStorage.getItem('jwt')}`,
+          'Content-Type': 'application/json',
+      }
+  });
+    const result = await response.json()
+    console.log(result)
+    setUserSearchResult(result);
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    await fetchUser();
+  }
+
   return (
     <div>
-      
+      <SearchForm 
+      onSubmit={handleSubmit}
+      searchItem={searchedUserName}
+      setSearchItem={setSearchedUserName}
+      searchType={"user"}/>
+      {userSearchResult ? (
+      <UserPage user={userSearchResult}/> ) : (null)}
     </div>
   )
 }
